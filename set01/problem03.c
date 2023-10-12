742d8b5ffcf585cd856a008fea68c880c7f4f784
Komal Suthar <sutharkomal507@gmail.com>
2023-10-12 22:33:20 +0530

    committed 10/12/2023, 10:33:19 PM

--- set01/problem03.c
+++ set01/problem03.c
@@ -7,7 +7,7 @@ int input() {
 int Add(int a, int b) {
     int sum;
     sum = a+b;
-    return sum;
+   
 } 
 void output(int a, int b, int sum) {
     printf("The sum of %d + %d is %d\n",a,b,sum);
