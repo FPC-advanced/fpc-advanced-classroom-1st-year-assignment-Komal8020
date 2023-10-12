f0d3eee9b1c4e3a0d395009fdfff68b8d13fe6b7
Komal Suthar <sutharkomal507@gmail.com>
2023-10-12 22:52:41 +0530

    committed 10/12/2023, 10:52:41 PM

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 void input(int *a, int *b) {
     printf("Enter the number:");
-    scanf("%d %d", a,b);
+    scanf("%d %d", &a,&b);
 }
 void Add(int a, int b, int *sum) {
   *sum = a +b;
 }
 
-void output(int a, int b, int sum) {
-    printf("The sum of %d + %d is %d\n", a, b, sum);
+void output(int sum) {
+    printf("The sum is %d\n", sum);
 }
 
 int main() {
     int a,b,sum;
     input(&a, &b);
     Add(a,b,&sum);
-    output(a,b,sum);
+    output(sum);
     return 0;
 }
--- set01/problem05.c
+++ set01/problem05.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
+int input() {
+    int a;
+    printf("Enter the number:");
+    scanf("%d",&a);
+    return a;
+}
 
+int compare(int a, int b, int c) {
+    int largest = a;
+    if(b>largest) {
+        largest = b;
+    }  if ( c > largest){
+        largest = c;
+    }
+    return largest;
+}
+
+void output(int a, int b, int c, int largest) {
+    printf("The largest of %d , %d and %d is %d\n",a,b,c, largest);
+}
+
+int main() {
+    int a, b, c,largest;
+    a = input();
+    b = input();
+    c = input();
+    largest = compare(a,b,c);
+    output(a,b,c,largest);
+    return 0;
+}
