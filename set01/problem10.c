ec36fc3efc876cfd1cccbfb165af7928b8ec26a0
Komal Suthar <sutharkomal507@gmail.com>
2023-10-17 22:23:44 +0530

    committed 10/17/2023, 10:23:43 PM

--- set01/problem10.c
+++ set01/problem10.c
@@ -1,39 +1,38 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<string.h>
+void input_two_strings(char *string1, char *string2);
+int stringcompare(char *string1, char *string2);
+void output(char *string1, char *string2, int result);
 
-// Function to input two strings
 void input_two_strings(char *string1, char *string2) {
-    printf("Enter the first string: ");
-    scanf("%s", string1);
-    printf("Enter the second string: ");
-    scanf("%s", string2);
+    printf("Enter two strings:");
+    scanf("%s %s", string1, string2);
 }
-
-// Function to compare two strings character by character
 int stringcompare(char *string1, char *string2) {
-     for(int i = 0; string1[i] != '\0' && string2[i] !='\0';i++) {
-        if(string1[i]> string2[i])
-     }
+    for(int i = 0; string1[i] != '\0' && string2[i] != '\0';i++) {
+    if(string1[i]>string2[i])
+      return 1;
+    if(string1[i] < string2[i])
+      return -1;
     }
-
-// Function to output the comparison result
+    return 0;
+}
 void output(char *string1, char *string2, int result) {
-    if (result < 0) {
-        printf("%s is greater\n",string1);
-    } else if (result > 0) {
-        printf("%s is greater\n",string2);
-    } else {
-       printf("Both are equal");
+    for(int i = 0;string1[i] != '\0' && string2[i] != '\0';i++) {
+        if(result > 0) {
+        printf("%s is greater",string1);}
+        else if(result < 0) {
+        printf("%s is greater", string2);}
+        else {
+        printf("Both are equal");
     }
 }
-
+}
 int main() {
-    char string1[100], string2[100];
-    input_two_strings(string1, string2);
-
-    int result = stringcompare(string1, string2);
-
-    output(string1, string2, result);
-
+    char string1, string2;
+    int result;
+    input_two_strings(string1,string2);
+    result = stringcompare(string1, string2);
+    output(string1, string2,result);
     return 0;
 }
