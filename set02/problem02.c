d368d8752358374f995613dca8f07baa12f3a7be
Komal Suthar <sutharkomal507@gmail.com>
2023-10-18 14:12:51 +0530

    committed 10/18/2023, 2:12:49 PM

--- set02/problem02.c
+++ set02/problem02.c
@@ -1,1 +1,38 @@
 #include<stdio.h>
+int input_side();
+int check_scalene(int a, int b, int c);
+void output(int a, int b, int c, int isscalene);
+int input_side() {
+    int a;
+    printf("Enter the side:");
+    scanf("%d",&a);
+    return a;
+}
+
+int check_scalene(int a, int b, int c) {
+    if(a != b && b != c && a != c) {
+        return 1;
+    }
+    else {
+        return -1;
+    }
+}
+
+void output(int a, int b, int c, int result) {
+    if(result > 0 ) {
+        printf("This is a scalene triangle");
+    } 
+    else if (result < 0){
+        printf("This is not a scalene triangle");
+    }
+}
+
+int main() {
+    int a,b,c,result;
+    a = input_side();
+    b = input_side();
+    c = input_side();
+    result = check_scalene(a,b,c);
+    output(a,b,c,result);
+    return 0;
+}
