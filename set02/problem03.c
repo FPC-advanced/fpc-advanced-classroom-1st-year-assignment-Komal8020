c1ffe9f46f5f2440842307527deeb24bbec6bd5e
Komal Suthar <sutharkomal507@gmail.com>
2023-10-19 15:57:00 +0530

    committed 10/19/2023, 3:56:59 PM

---
+++ set02/problem03.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int input_number();
+int is_composite(int n);
+void output(int n, int result);
+
+int input_number() {
+    int num;
+    printf("Enter the number:");
+    scanf("%d",&num);
+}
+
+int is_composite(int n) {
+    for(int i = 2) {
+
+    }
+}
