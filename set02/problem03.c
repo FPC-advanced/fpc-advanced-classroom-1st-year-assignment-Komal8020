fbc59cd7b20f13dc4a32e0b581f4e2506e2af29d
Komal Suthar <sutharkomal507@gmail.com>
2023-10-19 22:08:09 +0530

    committed 10/19/2023, 10:08:08 PM

--- set02/problem03.c
+++ set02/problem03.c
@@ -7,10 +7,35 @@ int input_number() {
     int num;
     printf("Enter the number:");
     scanf("%d",&num);
+    return num;
 }
 
 int is_composite(int n) {
-    for(int i = 2) {
+for(int i = 2; i<= 2*n; i++){
+   if (n%i == 0) {
+        return 1;
+   }
+}
+for(int j = 2; j <= 3*n; j++){
+    if (n%j == 0){
+        return 1;
+    } 
+}
+    return -1;
+}
 
+void output(int n,  int result) {
+    if (result > 0) {
+        printf("%d is a composite number:", n);
+    } else if (result < 0) {
+        printf("%d is not a composite number",n);
     }
 }
+int main() {
+    int num, result;
+    num = input_number();
+    result = is_composite(num);
+    output(num, result);
+    return 0;
+}
+
