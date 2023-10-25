9d205cd7386e37f602de48fe050a403a416a6156
Komal Suthar <sutharkomal507@gmail.com>
2023-10-25 19:02:34 +0530

    committed 10/25/2023, 7:02:33 PM

--- set02/problem05.c
+++ set02/problem05.c
@@ -1,17 +1,13 @@
 #include<stdio.h>
-int input();
-int find_gcd(int a, int b);
-void output(int a, int b, int gcd);
 int input() {
     int a;
     printf("Enter the number:");
     scanf("%d",&a);
     return a;
 }
-
 int find_gcd(int a, int b) {
-int gcd =1;
-    for (int i =0; i <= a && i <= b; i++) {
+int gcd;
+    for (int i =1; i <= a && i <= b; i++) {
         if (a % i == 0 && b % i == 0) {
             gcd = i;
         }
@@ -19,11 +15,7 @@ int gcd =1;
  return gcd;
 }
 void output(int a, int b, int gcd) {
-    if (gcd){
     printf("The gcd of %d and %d is %d\n", a, b, gcd);
-} else {
-    printf("No gcd is found for %d and %d", a,b);
-}
 }
 int main() {
     int a, b,gcd;
