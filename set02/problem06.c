31f2d8258244a3c55e8ffe3c18b9c4ffb26d88fa
Komal Suthar <sutharkomal507@gmail.com>
2023-10-30 09:31:32 +0530

    committed 10/30/2023, 9:31:30 AM

--- set02/problem06.c
+++ set02/problem06.c
@@ -8,12 +8,13 @@ void input_string(char *a) {
 }
 
 void str_reverse(char *str, char *rev_str) {
-  int len = strlen(str);
-  for(int i = 0; i<len/2; i++) {
-    char b = str[i];
-    str[i] = str[len -1-i];
-    str[len-1-i] = b;
-  }
+  int length = strlen(str);
+  int i, j;
+    for (i = length - 1, j = 0; i >= 0; i--, j++) {
+        rev_str[j] = str[i];
+    }
+    rev_str[j] = '\0';
+
 }
 
 void output(char *a, char *reverse_a) {
