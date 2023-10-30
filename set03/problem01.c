aea329d548080b3751872012832ae29334237ec0
Komal Suthar <sutharkomal507@gmail.com>
2023-10-30 23:01:57 +0530

    committed 10/30/2023, 11:01:56 PM

--- set03/problem01.c
+++ set03/problem01.c
@@ -13,16 +13,16 @@ void input(float *x1, float *y1, float *x2, float *y2) {
    scanf("%f",y2);
 }
 float find_distance(float x1, float y1, float x2, float y2) {
-   float distance = sqrt(pow(x2-x1, 2)- pow(y2-y1, 2));
+   float distance = sqrt(pow(x2-x1, 2) + pow(y2-y1, 2));
    return distance;
 }
-void output(float *x1, float *y1, float *x2, float *y2, float distance) {
+void output(float x1, float y1, float x2, float y2, float distance) {
     printf("The distace between point A (%f,%f) and B(%f,%f) is %f", x1,y1, x2, y2, distance);
 }
 int main() {
     float x1,x2,y1, y2,distance;
     input(&x1,&x2,&y1,&y2);
     distance = find_distance(x1,y1,x2,y2);
-    output(&x1,&y1,&x2,&y2,distance);
+    output(x1,y1,x2,y2,distance);
     return 0;
 }
