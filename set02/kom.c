949ed9591552f454f9eee6d20ad913799901a2d4
Komal Suthar <sutharkomal507@gmail.com>
2023-10-30 10:19:33 +0530

    committed 10/30/2023, 10:19:32 AM

--- set02/kom.c
+++ set02/kom.c
@@ -1,38 +1,44 @@
- //Write a program to find the triangle with smallest area in n given triangles.
 #include<stdio.h>
 typedef struct _triangle {
-	float base;
-    float altitude;
-    float area;
+	float base, altitude, area;
 } Triangle;
+int input_n();
+Triangle input_triangle();
+void input_n_triangles(int n, Triangle t[n]);
+void find_area(Triangle *t);
+void find_n_areas(int n, Triangle t[n]);
+Triangle find_smallest_triangle(int n, Triangle t[n]);
+void output(int n, Triangle t[n], Triangle smallest);
+
 int input_n() {
     int n;
     printf("Enter the number of triangle:");
-    scanf("%d",&n);
+    scanf("%d", &n);
     return n;
 }
 Triangle input_triangle() {
     Triangle t;
     printf("Enter the base:");
-    scanf("%f",&t.base);
+    scanf("%f", &t.base);
 
     printf("Enter the altitude:");
     scanf("%f",&t.altitude);
-    return t;
+    return  t;
 }
-void input_n_triangles(int n, Triangle t[n]) {
-    for(int i = 0; i <= n; i++) {
-        printf("Input %d no. of triangle:", i+1);
-        scanf("%f",t[i]);
+void input_n_triangles(int n, Triangle t[n]){
+    for(int i = 0;i <= n; i++) {
+        printf("Enter %d number of triangle", i+1);
+        scanf("%d",t[i]);
     }
 }
 void find_area(Triangle *t) {
-    t->area = (t->altitude * t->base) * 0.5;
+   t->area = t->base * t->altitude * 0.5;
 }
 void find_n_areas(int n, Triangle t[n]) {
-     for(int i = 0; i <= n; i++) {
-     find_area(&t[i]);
-  }
+    for (int i = 0; i <= n; i++) {
+        printf("The area of %d no. of triangle:",n);
+        find_area(&t[i]);
+    }
 }
 Triangle find_smallest_triangle(int n, Triangle t[n]) {
     Triangle smallest = t[0];
@@ -44,7 +50,7 @@ for(int i = 1; i<= n; i++) {
 return smallest;
 }
 void output(int n, Triangle t[n], Triangle smallest) {
-    printf("Area of n number of triangle :\n");
+    printf("Details of the triangle:\n");
     for(int i = 0; i <= n; i++) {
         printf("Triangle %d: Base = %f, Altitude = %f, Area = %f\n", i+1,t[i].base, t[i].altitude,t[i].area);
         printf("Triangle with smallest area : Base = %f, Altitude = %f, Area = %f\n", smallest.base, smallest.altitude,smallest.area);
