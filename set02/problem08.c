1894c624bf3d673a5dc1a3c7ee4880859789a1b9
Komal Suthar <sutharkomal507@gmail.com>
2023-10-30 22:36:35 +0530

    committed 10/30/2023, 10:36:34 PM

--- set02/problem08.c
+++ set02/problem08.c
@@ -21,7 +21,7 @@ Triangle input_triangle() {
     return t;
 }
 void input_n_triangles(int n, Triangle t[]) {
-    for(int i = 0; i <= n; i++) {
+    for(int i = 0; i < n; i++) {
         printf("Enter %d no. of triangle:", i);
         t[i] = input_triangle();
     }
@@ -31,13 +31,13 @@ void find_area(Triangle *t) {
     t->area = (t->altitude * t->base) * 0.5;
 }
 void find_n_areas(int n, Triangle t[]) {
-     for(int i = 0; i <= n; i++) {
+     for(int i = 0; i < n; i++) {
      find_area(&t[i]);
   }
 }
 Triangle find_smallest_triangle(int n, Triangle t[]) {
     Triangle smallest = t[0];
-for(int i = 1; i<= n; i++) {
+for(int i = 1; i< n; i++) {
     if(t[i].area < smallest.area) {
         smallest = t[i];
     }
@@ -46,7 +46,7 @@ return smallest;
 }
 void output(int n, Triangle t[], Triangle smallest) {
     printf("Area of %d  triangle :\n",n);
-    for(int i = 0; i <= n; i++) {
+    for(int i = 0; i < n; i++) {
      printf("Triangle no. %d\n Base:%f\n Altitude: %f\n Area: %f\n", i, t[i].base, t[i].altitude, t[i].area); 
      printf("Smallest Triangle:\nBase: %f\n, Altitude: %f\n, Area: %f\n", smallest.base, smallest.altitude, smallest.area);
     }
