d1d4fd28b1fce08a8d96a0321ad10d3193e511e4
Komal Suthar <sutharkomal507@gmail.com>
2023-10-31 23:29:15 +0530

    committed 10/31/2023, 11:29:14 PM

--- set03/problem03.c
+++ set03/problem03.c
@@ -9,7 +9,7 @@ int input_number() {
     return n;
 }
 int is_prime(int n) {
-    for (int i = 0; i <= n; i++) {
+    for (int i = 2; i <= n; i++) {
         if (n%i == 0) {
             return 1;
         } else {
