a52fd3de809655e221afe43950559fdc6a209df3
Komal Suthar <sutharkomal507@gmail.com>
2023-11-18 21:10:34 +0530

    committed 11/18/2023, 9:10:33 PM

--- set03/problem07.c
+++ set03/problem07.c
@@ -1,4 +1,6 @@
+//Write a program to find the length of a line
 #include<stdio.h>
+#include<math.h>
 typedef struct point {
     float x, y;
 } Point;
@@ -13,6 +15,30 @@ Line input_line();
 void find_length(Line *l);
 void output(Line l);
 Point input_point() {
-    float x, y;
-    printf("Enter the p");
+    Point p;
+    printf("Enter x and y coordinates:");
+    scanf("%f %f",&p.x, &p.y);
+    return p;
+}
+Line input_line() {
+    Line l;
+    printf("Enter the coordinates for p1:\n");
+    scanf("%f",l.p1);
+    printf("Enter the coordinates for p2:\n");
+    scanf("%f",l.p2);
+    return l;
+}
+void find_length(Line *l) {
+    l->distance = sqrt(pow((l->p2.x - l->p1.x),2) +pow((l->p2.y - l->p1.y),2));
+}
+void output(Line l) {
+  printf("Coordinates of p1:(%f,%f)\n",l.p1.x, l.p1.y);
+  printf("Coordinates of p2:(%f,%f)\n",l.p2.x, l.p2.y);
+  printf("Length of a line is:%f\n",l.distance);
+}
+int main() {
+    Line l = input_line();
+    find_length(&l);
+    output(l);
+    return 0;
 }
