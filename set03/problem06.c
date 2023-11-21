1f36dc09f71a9a3b2401b2846f49f8f3147e8ef2
Komal Suthar <sutharkomal507@gmail.com>
2023-11-21 12:01:36 +0530

    committed 11/21/2023, 12:01:35 PM

--- set03/problem06.c
+++ set03/problem06.c
@@ -1,23 +1,33 @@
 //Write a program to find the index of a substring of a string
 #include<stdio.h>
-void input_string(char* a, char* b);
-int sub_str_index(char* string, char* substring);
-void output(char *string, char *substring, int index);
-void input_string(char* a, char* b) {
+void input_string(char *a, char *b) {
     printf("Enter the string a:");
     scanf("%s",a);
     printf("Enter the string b:");
     scanf("%s",b);
 }
-int sub_str_index(char* string, char* substring) {
-     for (int i = 0; i <= *string; i++) {
-       
-     }
+int sub_str_index(char  *string, char *substring) {
+  int len_string = strlen(string);
+  int len_substring = strlen(substring);
+  for (int i = 0; i<= len_string-len_substring; i++){
+    int j;
+    for(j = 0; i<= len_substring;j++) {
+        if(string[i+j] != substring[j]) {
+            break;
+        }
+    }
+    if(j==len_substring) {
+        return i;
+    }
+  }
+  return -1;
 }
 void output(char *string, char *substring, int index) {
-    printf("Enter the index of the string:%d",index);
-    printf("The string is %s\n",string);
-    printf("The substring of the string is %s\n",substring);
+    if(index != -1) {
+    printf("Sustring '%s' found in the main string '%s' at index %d.\n ",substring, string,index);
+    } else {
+        printf("Substring %s is not found in the main string.",substring);
+    }
 }
 int main() {
     char *a, *b, *string, *substring;
