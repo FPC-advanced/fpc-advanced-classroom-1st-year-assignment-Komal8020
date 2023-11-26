b73e9640138a57c8ee67808643555da8a633f190
Komal Suthar <sutharkomal507@gmail.com>
2023-11-26 22:17:33 +0530

    committed 11/26/2023, 10:17:32 PM

--- set04/problem05.c
+++ set04/problem05.c
@@ -1,10 +1,5 @@
 //Write a program to find the index of the largest number in an array
 #include<stdio.h>
-int input_size();
-void input_array(int n, int a[]);
-int find_largest_index(int n, int a[]);
-void output(int index);
-
 int input_size() {
     int n;
     printf("Enter the size of an array:");
@@ -13,25 +8,25 @@ int input_size() {
 }
 void input_array(int n, int a[]) {
     
-    for(int i =0; i<= n; i++) {
-       printf("Enter the %d element of the array:",i+1);
+    for(int i =0; i< n; i++) {
+       printf("Enter the %d element of the array:",i);
         scanf("%d",&a[i]);
     }
 }
 int find_largest_index(int n, int a[]) {
-    int index;
+    int maxIndex=0;
   for( int i = 0; i <n; i++) {
-    if (index < a[i])
-    index = a[i];
+    if (a[maxIndex] < a[i])
+    maxIndex = i;
   }
-  return index;
+  return maxIndex;
 }
 void output(int index) {
-    printf("The index of the largest number in an array:", index);
+    printf("The index of the largest number in an array:%d\n", index);
 } 
 int main() {
     int n = input_size();
-    int array[n];
+    int array[1000];
     input_array(n,array);
     int largestindex= find_largest_index(n,array);
     output(largestindex);
