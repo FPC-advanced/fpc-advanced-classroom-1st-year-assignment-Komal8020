fe6e9cbe2dea73b21a4a3ca96636d6201ae64f73
Komal Suthar <sutharkomal507@gmail.com>
2023-11-29 10:25:26 +0530

    committed 11/29/2023, 10:25:25 AM

--- set04/problem03.c
+++ set04/problem03.c
@@ -1,4 +1,5 @@
  //Write a program to find the `nCr` of given n and r
+ #include<stdio.h>
 void input_n_and_r(int *n, int *r) {
     printf("Enter the value for n:");
     scanf("%d", n);
@@ -6,9 +7,12 @@ void input_n_and_r(int *n, int *r) {
     scanf("%d",r);
 }
 int nCr(int n, int r) {
-    
+    if (r == 0 || r == n) {
+        return 1;
+    } else {
+        return nCr(n - 1, r - 1) + nCr(n - 1, r);
+    }
 }
-
 void output(int n, int r, int result) {
     printf("The Combination of C(%d,%d) is %d", n,r, result);
 }
