4519fce7c8881dbd86facd54128bdf9942b9bb6a
Komal Suthar <sutharkomal507@gmail.com>
2023-12-05 00:07:43 +0530

    committed 12/5/2023, 12:07:43 AM

--- set03/problem08.c
+++ set03/problem08.c
@@ -1,4 +1,6 @@
 // Write a program to find the perimeter of a polygon
+#include<stdio.h>
+#include<math.h>
 typedef struct point {
     float x,y;
 } Point;
@@ -20,20 +22,40 @@ int input_n() {
     return n;
 }
 Point input_point(char *prompt_msg) {
- 
+ Point p;
+ printf("%s",prompt_msg);
+ scanf("%f %f", &p.x, &p.y);
+ return p;
 }
 int input_polygon(Polygon *p) {
-
+  int i;
+  p->sides = input_n();
+  for(i = 0; i < p->sides; i++) {
+    p->p[i] = input_point("Enter vertex (x,y):");
+  }
+ 
 }
 float find_distance(Point a, Point b) {
-
+ return sqrt(pow(b.x - a.x,2) + pow(b.y - a.y,2));
 }
 void find_perimeter(Polygon* p) {
-
+p->perimeter = 0;
+for(int i = 0; i < p->sides; i++) {
+    int next_index = (i+1) % p->sides;
+    p->perimeter += find_distance(p->p[i], p->p[next_index]);
+}
 }
 void output(Polygon p) {
-
+printf("Perimeter of the polygon is: %f\n",p.perimeter);
 }
 int main() {
-    
+    Polygon p;
+    p.sides = input_n();
+    if (input_polygon(&p)) {
+        find_perimeter(&p);
+        output(p);
+    } else {
+        printf("Invalid input for polygon points.\n");
+    }
+    return 0;
 }
--- set04/problem06.c
+++ set04/problem06.c
@@ -9,6 +9,21 @@ void input_string(char *a) {
     scanf("%s", a);
 }
 int count_words(char *string) {
- 
-    
+ int count = 0;
+ char *token = strtok(string,"");
+ while (token != NULL) {
+    count++;
+    token = strtok(NULL,"");
+ }
+ return count;
+}
+void output(char *string, int no_words) {
+    printf("The number of words in \"%s\" is %d.\n",string,no_words);
+}
+int main() {
+    char string[1000];
+    input_string(string);
+    int no_words = count_words(string);
+    output(string, no_words);
+    return 0;
 }
