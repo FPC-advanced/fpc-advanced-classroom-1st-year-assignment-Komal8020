6eb8a106dd4dc768aee468757fbdb254dfa1064c
Komal Suthar <sutharkomal507@gmail.com>
2023-12-19 14:04:27 +0530

    committed 12/19/2023, 2:04:25 PM

---
+++ PSC/problem02.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int input_array_size() {
+    int n;
+    printf("Enter the size of the array:");
+    scanf("%d",&n);
+    return n;
+}
+int get_array_values(int n, int a[]) {
+    printf("Enter the %d number of the array.\n",n);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d",&a[i]);
+    }
+}
+int is_factorial(int n, int a[]) {
+    int factorial;
+  for (int i = 1; i < n; i++) {
+    n == 1 && n % 2== 0;
+  }
+  return is_factorial(n,a);
+}
+int get_factorial(int n, int a[], int f[]) {
+    for (int i = 0; i < n; i++) {
+        f[i] = is_factorial(n,a);
+    }
+}
+void output(int n, int f[]) {
+ printf("The factorial number in an array of number is %d",f);
+}
+int main() {
+    int n = input_array_size();
+    int a[1000];
+    get_array_values(n,a);
+    is_factorial(n,a);
+    int f[1000];
+    get_factorial(n,a,f);
+    output(n,f);
+    return 0;
+}
