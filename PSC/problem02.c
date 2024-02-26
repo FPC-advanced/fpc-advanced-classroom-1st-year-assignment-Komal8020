f16aa0f4906690dc56f84493291773535d8b149f
Komal Suthar <sutharkomal507@gmail.com>
2024-02-27 00:47:26 +0530

    committed 2/27/2024, 12:47:26 AM

--- PSC/problem02.c
+++ PSC/problem02.c
@@ -7,36 +7,43 @@ int input_array_size() {
 }
 int get_array_values(int n, int a[]) {
     printf("Enter the %d number in the array.\n",n);
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         scanf("%d",&a[i]);
     }
 }
 int is_factorial(int n) {
     int fact = 1;
     int i=1;
-  while (fact < n) {
+    while (fact < n) {
       i++;
       fact *= i;
     }   
   return fact;
+  if(fact == n) {
+    return n;
+  } else {
+    return 0;
+  }
 }
 void get_factorial(int n, int a[], int f[]) {
-    for(int i = 2; i < n; i++) {
-      f[i] == is_factorial(a[i]);
+    for(int i = 0; i < n; i++) {
+      f[i] = is_factorial(a[i]);
     }  
 }   
 
 void output(int n, int f[]) {
    printf("Factorials present in array:\n");
-   for (int i = 2; i < n; i++) {
-    printf("%d\n",f[i]);
-   }
+   for (int i = 0; i < n; i++) {
+     if (f[i] != 0) {
+      printf("%d\n",f[i]);
+    }
+  }
 }
 int main() {
     int n = input_array_size();
-    int a[1000];
+    int a[n];
     get_array_values(n,a);
-    int f[1000];
+    int f[n];
     get_factorial(n,a,f);
     output(n,f);
     return 0;
--- PSC/problem03.c
+++ PSC/problem03.c
@@ -1,38 +1,116 @@
+/*. A Fritacole has attributes: name, height,  integrity, interest, ability, discipline.
+There were two teams of Fritacoles playing a freyball game.
+A freyball game consists of points and fouls
+Each point is scored by a fritacole.
+Each foul is committed by a fritacole.
+The team that scores more goals wins the game.
+If the goals are the same, the team that commits less fouls wins the game.
+
+a. Write a function to verify whether the better team won a game?
+Note that taller the person, the more chance that the fritacole may score a point.
+
+b, Write a function to convert a fritacole into a string.
+
+c. Write a function to convert a string into a fritacole.
+*/
 #include<stdio.h>
-typedef struct fritacole {
-char name[100];
-float height;
-} Fritacole;
-typedef struct foul {
-    char name [100];
-    int ability;
-}fouls;
-typedef struct  point 
-{
+typedef struct _fritacole {
+    int id;
     char name[100];
     float height;
-}points;
+    int integrity;
+    int ability;
+    int discipline;
+    int interest;
+} fritacole;
+typedef struct _foul {
+    int id;
+}foul;
+typedef struct  _point 
+{
+    int id;
+}point;
 typedef struct Team {
-char playername[100];
-int numplayers;
-Fritacole players[100];
-fouls foul[100];
-points point[100];
-int num_fouls;
-int num_points;
+    char teamname[100];
+    int numplayers;
+    fritacole players[100];
 } team;
 
 typedef struct Game {
     team teams[2];
+    int num_points;
+    point p[100];
+    int num_fouls;
+    foul f[100]
 }game;
 
-void frita_name(Fritacole *a) {
-    printf("Enter the name of the fritacole:");
-    scanf("%s",a->name);
-    printf("Enter the height:");
-    scanf("%f",a->height);
+
+fritacole input_fritacole()
+{
+   fritacole f;
+   printf("Enter the fritacole ID:");
+   scanf("%d",&f.id);
+   printf("Enter the name of  fritacole:");
+   scanf("%s", &f.name);
+   printf("Enter the height of  fritacole:");
+   scanf("%f",&f.height);
+   printf("Enter the integrity of fritacole:");
+   scanf("%d",&f.integrity);
+   printf("Enter the ability of fritacole:");
+   scanf("%d",&f.ability);
+   printf("Enter the discipline of fritacole:");
+   scanf("%d",&f.discipline);
+   printf("Enter the interest of fritacole:");
+   scanf("%d",&f.interest);
+   return f;
+}
+
+void input_n_fritacoles(int n, fritacole f[n])
+{
+   for (int i = 0; i < n; i++) {
+    printf("Enter the details of fritacole for %d\n", i+1);
+    f[i] = input_fritacole();
+   }
 }
 
 
+team input_team()
+{
+    team t;
+    printf("Enter the name of the team:");
+    scanf("%s",&t.teamname);
+    printf("Enter the number of players in a team:");
+    scanf("%d",&t.numplayers);
+    return t;
+}
 
+game input_game()
+{
+   game g;
+   printf("Enter details of Team 1:\n");
+   //scanf("%d",&g.teams) or g.team1= input_team
+   printf("Enter details of Team 2:\n");
+
+   printf("Enter the number of points:");
+   scanf("%d",&g.num_points);
+   printf("Enter the number of fouls:");
+   scanf("%d",&g.num_fouls);
+   return g;
+}
+
+point input_point()
+{
+  point p;
+  printf("Enter the point scored by fritacole id:");
+  scanf("%d",&p.id);
+  return p;
+}
+
+foul input_foul()
+{
+    foul f;
+    printf("Enter the foul scored by fritacole id:");
+    scanf("%d",&f.id);
+    return f;
+}
 
